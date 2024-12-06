4863c2cc1eb19e64eadf2216eaa41c2de825b210
Andrew Brisbin <abrisbin@andculture.com>
2024-12-05 22:20:55 -0500

    improves performance from 11 seconds down to 20 milliseconds.

--- 2024/05/solution.ts
+++ 2024/05/solution.ts
@@ -35,191 +35,73 @@ const tests = {
 import { log, logList } from "../../tools.ts";
 import { puzzle } from "../../puzzle.ts";
 
-
-const findAnswers = (input: string, entries: string[][] | string[][][]) => {
-	const parts = input.split("\n\n");
-
-	const sortingRules = parts[0].split("\n");
-	const validPageSets = [] as string[];
-	const invalidPageSets = [] as string[];
-
-	const pages = parts[1].split("\n");
-	log ("pagecount:" + pages.length);
-
-	const start = performance.now();
-	pages.forEach((page, index) => {
-		const pageNumbers = page.split(",");
-		let sortOk = true;
-
-		const relevantRules = sortingRules.filter(rule => pageNumbers.some(num => rule.includes(num))); // 11 seconds
-		// const relevantRules = sortingRules; // 37 seconds
-		let sortFailed = true;
-
-		let i = 0;
-		while (sortFailed && i<relevantRules.length) {
-
-			// if(relevantRules.some(rule => {
-			// 	// does this process every single one?
-			// 	const [first, second] = rule.split("|");
-			// 	const firstIndex = pageNumbers.indexOf(first);
-			// 	const secondIndex = pageNumbers.indexOf(second);
-			// 	if(firstIndex === -1 || secondIndex === -1){
-			// 		return false;
-			// 	}
-			// 	return firstIndex > secondIndex;
-			// })){
-			// 	sortOk = false;
-			// }
-
-			if (relevantRules.every(rule => {
-				const [first, second] = rule.split("|");
-				const firstIndex = pageNumbers.indexOf(first);
-				const secondIndex = pageNumbers.indexOf(second);
-				if(firstIndex === -1 || secondIndex === -1){
-					return true;
-				}
-				return firstIndex < secondIndex;
-			})){
-				sortFailed = false;
-			}
-
-			i++;
-		}
-
-		if (sortFailed) {
-			invalidPageSets.push(page);
-		} else {
-			validPageSets.push(page);
-		}
-
-	});
-
-	const end = performance.now();
-	log ("time to process:" + (end-start));
-
-	log ("invalidPageSets.length:" + invalidPageSets.length);
-
-	let part2 = 0;
-	// TODO  determine time it takes to sort the invalid pagesets
-
-
-	const sortedPageSets = invalidPageSets.map(pageSet => {
-		return sortPageset (pageSet, sortingRules).join(",");
-	});
-	part2 = sumMiddleNumber(sortedPageSets);
-
-	const answers = { first: sumMiddleNumber(validPageSets), second: part2 };
-
-	// Create a Set of all the numbers and the numbers they need to precede.
-	// Then loop over each row and for each number, check if if there are any numbers in the remaining portion of the list that is need to be before.
-
-	return answers;
-};
-
-const findMyAnswers = (input: string, entries: string[][] | string[][][]) => {
-	const parts = input.split("\n\n");
-
-	const sortingRules = parts[0].split("\n");
-	const sortingRulesMap = createSortingMap(sortingRules);
-	log ("sortMap for 75:", sortingRulesMap.get("75"));
-
-	const validPageSets = [] as string[];
-	const invalidPageSets = [] as string[];
-
-	const pageLists = parts[1].split("\n");
-	log ("pageListCount:" + pageLists.length);
-
-	pageLists.forEach((pages, index) => {
-		log ("page:" + index);
-		const pageNumbers = pages.split(",");
-		let sortOk = true;
-		
-		let i = 0;
-
-		while (sortOk && i<pageNumbers.length) {
-			const pageNumber = pageNumbers[i];
-			const numbersThatPageMustBeBefore = sortingRulesMap.get(pageNumber);
-			const numbersThatPageMustBeAFter = sortingRulesMap.get(pageNumber);
-
-			const remainingPages = pageNumbers.slice(i+1);
-			// log ("pgNumber" + pageNumber);
-			// log ("numbers that MUST be after:" + numbersThatMustBeAfter);
-			sortOk = remainingPages.some(remainingPage => {
-				if(numbersThatPageMustBeAFter && numbersThatPageMustBeAFter.includes(remainingPage)){
-					// log ("remainingPage:" + remainingPage);
-					return true;
-				}
-				return false;
-			});
-			// if(numbersToPrecede){
-			// 	const numbersToPrecedeIndexes = numbersToPrecede.map(num => pageNumbers.indexOf(num));
-			// 	if(numbersToPrecedeIndexes.some(index => index > i)){
-			// 		sortOk = false;
-			// 	}
-			// }
-			i++;
-		}
-
-		if (sortOk) {
-			validPageSets.push(pages);
-		} else {
-			invalidPageSets.push(pages);
-		}
-
-	});
-
-	log ("invalidPageSets:" + invalidPageSets);
-
-	let part2 = 0;
-	// const sortedPageSets = invalidPageSets.map(pageSet => {
-	// 	return sortPageset (pageSet, sortingRules).join(",");
-	// });
-	// part2 = sumMiddleNumber(sortedPageSets);
-
-	const answers = { first: sumMiddleNumber(validPageSets), second: part2 };
-
-	// Create a Set of all the numbers and the numbers they need to precede.
-	// Then loop over each row and for each number, check if if there are any numbers in the remaining portion of the list that is need to be before.
-
-	return answers;
-};
-
-const createSortingMap = (sortingRules: string[]): Map<string, string[]> => {
-	const sortingRulesMap = new Map<string, string[]>();
-	sortingRules.forEach(rule => {
-		const [first, second] = rule.split("|");
-		if(sortingRulesMap.has(first)){
-			sortingRulesMap.get(first)?.push(second);
-		} else {
-			sortingRulesMap.set(first, [second]);
-		}
-	});
-	return sortingRulesMap;
+interface PageLists {
+	validPageLists: string[];
+	invalidPageLists: string[];
 }
 
-export const sortPageset = (page: string, rules: string[]): string[] => {
-	const pageNumbers = page.split(",");
-	const pageNumbersCopy = [...pageNumbers];
-	const relevantRules = rules.filter(rule => pageNumbers.some(num => rule.includes(num)));
+const findAnswers = (input: string) => {
+	const parts = input.split("\n\n");
 
-	let sortOk = false;
-	while (!sortOk) {
-		sortOk = true;
+	const sortingRules = parts[0].split("\n");
+
+	const pages = parts[1].split("\n");
+
+	const sortedPageLists = perf(() => categorizePageListsWithSorting(pages, sortingRules)); // 25 milliseconds
+
+	const answers = { first: sumMiddleNumber(sortedPageLists.validPageLists), second: sumMiddleNumber(sortedPageLists.invalidPageLists) };
+
+	return answers;
+};
+
+const categorizePageListsWithSorting = (pageLists: string[], rules: string[]): PageLists => {
+	const validPageLists = [] as string[];
+	const invalidPageLists = [] as string[];
+	const splitRules = rules.map(rule => rule.split("|"));
+
+	pageLists.forEach((pageList) => {
+
+		const sortedPageSet = sortPageset(pageList, splitRules).join(",");
+		if ( pageList === sortedPageSet ) {
+			validPageLists.push(pageList);
+		} else {
+			invalidPageLists.push(sortedPageSet);
+		}
+
+	});
+
+	return {validPageLists: validPageLists, invalidPageLists: invalidPageLists};
+}
+
+export const sortPageset = (page: string, rules: string[][]) : string[] => {
+	const pageNumbers =page.split(",");
+
+	// only include rules that where both numbers are in the pageNumbers list.
+	const relevantRules = rules.filter(rule => {
+		const [firstNumber, secondNumber] = rule;
+		return pageNumbers.includes(firstNumber) && pageNumbers.includes(secondNumber);
+	});
+
+	let sorted = false;
+	while (!sorted) {
+		sorted = true;
 		relevantRules.forEach(rule => {
-			const [first, second] = rule.split("|");
-			const firstIndex = pageNumbersCopy.indexOf(first);
-			const secondIndex = pageNumbersCopy.indexOf(second);
-			if(firstIndex === -1 || secondIndex === -1){
-				return;
-			}
+			const [firstNumber, secondNumber] = rule;
+			const firstIndex = pageNumbers.indexOf(firstNumber);
+			const secondIndex = pageNumbers.indexOf(secondNumber);
 			if(firstIndex > secondIndex){
-				pageNumbersCopy[firstIndex] = second;
-				pageNumbersCopy[secondIndex] = first;
-				sortOk = false;
+				swap(pageNumbers, firstIndex, secondIndex);
+				sorted = false;
 			}
 		});
 	}
-	return pageNumbersCopy;
+	return pageNumbers;
+}
+
+export const swap = (arr: string[], index1: number, index2: number) => {
+	const temp = arr[index1];
+	arr[index1] = arr[index2];
+	arr[index2] = temp;
 }
 
 export const sumMiddleNumber = (arr: string[]): number => {
@@ -237,18 +119,90 @@ export const getMiddleOfArray = (arr: string[]): number => {
 	return parseInt(arr[middle]);
 }
 
+// create a method that receives a function that will execute the function and log start and end time.
+// deno-lint-ignore ban-types
+const perf = (func: Function) => {
+	const start = performance.now();
+	const result = func();
+	const end = performance.now();
+	log ("time:" + (end-start));
+	return result;
+}
+
+const categorizePageLists = (pageLists: string[], rules: string[]): PageLists => {
+	const validPageLists = [] as string[];
+	const invalidPageLists = [] as string[];
+
+	const start = performance.now();
+
+	pageLists.forEach((pageList) => {
+		const pageNumbers = pageList.split(",");
+
+		const relevantRules = rules.filter(rule => pageNumbers.some(num => rule.includes(num)));
+		if (isPageListSorted(pageNumbers, relevantRules)) {
+			validPageLists.push(pageList);
+		} else {
+			invalidPageLists.push(pageList);
+		}
+	});
+	const end = performance.now();
+	log ("time:" + (end-start));
+
+	return {validPageLists: validPageLists, invalidPageLists: invalidPageLists};
+}
+
+export const isPageListSorted = (pageList: string[], rules: string[]): boolean => {
+	let sortFailed = true;
+
+	const relevantRules = rules.filter(rule => pageList.some(num => rule.includes(num))); // 11 seconds
+
+	let i = 0;
+	while (sortFailed && i<relevantRules.length) {
+
+		// This seems unnecessary and will process EVERY record.
+		// if(relevantRules.some(rule => {
+		// 	// does this process every single one?
+		// 	const [first, second] = rule.split("|");
+		// 	const firstIndex = pageNumbers.indexOf(first);
+		// 	const secondIndex = pageNumbers.indexOf(second);
+		// 	if(firstIndex === -1 || secondIndex === -1){
+		// 		return false;
+		// 	}
+		// 	return firstIndex > secondIndex;
+		// })){
+		// 	sortOk = false;
+		// }
+
+		if (relevantRules.every(rule => {
+			const [first, second] = rule.split("|");
+			const firstIndex = pageList.indexOf(first);
+			const secondIndex = pageList.indexOf(second);
+			if(firstIndex === -1 || secondIndex === -1){
+				return true;
+			}
+			return firstIndex < secondIndex;
+		})){
+			sortFailed = false;
+		}
+
+		i++;
+	}
+
+	return !sortFailed;
+}
+
 const testPart1 = async (input: string): Promise<boolean> => {
 	if(tests.first === 0){ return false; }
 	const puzzle_input = await puzzle.parseInput(input);
 	const entries = puzzle_input.blocks.length > 0 ? puzzle_input.blocks : puzzle_input.blocks[0];
-	const answers = findAnswers(input, entries);
+	const answers = findAnswers(input);
 	return answers.first === tests.first;
 };
 const solvePart1 = async (): Promise<number> => {
 	if(tests.first === 0){ return 0; }
 	const puzzle_input = await puzzle.parseInput();
 	const entries = puzzle_input.blocks.length > 0 ? puzzle_input.blocks : puzzle_input.blocks[0];
-	const answers = findAnswers(puzzle_input.input,entries);
+	const answers = findAnswers(puzzle_input.input);
 	return answers.first;
 };
 
@@ -256,7 +210,7 @@ const solveBoth = async (): Promise<{first: number, second: number}> => {
 	if(tests.first === 0){ return {first: 0, second: 0}; }
 	const puzzle_input = await puzzle.parseInput();
 	const entries = puzzle_input.blocks.length > 0 ? puzzle_input.blocks : puzzle_input.blocks[0];
-	const answers = findAnswers(puzzle_input.input,entries);
+	const answers = findAnswers(puzzle_input.input);
 	return answers;
 };
 
@@ -264,14 +218,14 @@ const testPart2 = async (input: string): Promise<boolean> => {
 	if(tests.second === 0){ return false; }
 	const puzzle_input = await puzzle.parseInput(input);
 	const entries = puzzle_input.blocks.length > 0 ? puzzle_input.blocks : puzzle_input.blocks[0];
-	const answers = findAnswers(input, entries);
+	const answers = findAnswers(input);
 	return answers.second === tests.second;
 };
 const solvePart2 = async (): Promise<number> => {
 	if(tests.second === 0){ return 0; }
 	const puzzle_input = await puzzle.parseInput();
 	const entries = puzzle_input.blocks.length > 0 ? puzzle_input.blocks : puzzle_input.blocks[0];
-	const answers = findAnswers(puzzle_input.input,entries);
+	const answers = findAnswers(puzzle_input.input);
 	return answers.second;
 };
 
@@ -300,8 +254,5 @@ check = part2_correct ? '✅' : '❌';
 
 const both = await solveBoth();
 
-// const part1 = part1_correct ? await solvePart1(): null;
 log("    part 1: ", both.first, check);
-
-// const part2 = part2_correct ? await solvePart2(): null;
 log("    part 2: ", both.second, check);
